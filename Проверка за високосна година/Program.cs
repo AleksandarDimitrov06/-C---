227723aa68bd362ca7a4e718278490457c23bd33
Александър Димитров <alexander.v.dim@gmail.com>
2024-02-16 18:23:10 +0200

    Changes on code

--- Проверка за високосна година/Program.cs
+++ Проверка за високосна година/Program.cs
@@ -1,8 +1,42 @@
-﻿Console.WriteLine("Enter year: ");
-string input = Console.ReadLine();
-int year=0;
-if(int.Parse(input)>0) year = int.Parse(input);
-else {Console.WriteLine("Wrong input!");Environment.Exit(1);}
+﻿//променлива за проверка на входните данни
+bool Correct_Input = false;
+
+//променлива, която ще съхранява входните данни
+string input = null;
+
+//променлива, която ще съхранява годината
+int year = 0;
+
+//цикъл за година
+while (!Correct_Input)
+{
+    //подканваме потребителя да въведе година
+    Console.WriteLine("Enter year: ");
+
+     input= Console.ReadLine();
+
+    //проверка на данните
+    if (int.TryParse(input, out year) && (int.Parse(input) > 0))
+    {
+        //край на цикъла
+        Correct_Input=true;
+
+    }
+
+    //в противен случай
+    else
+    {
+        //показваме съобщение за грешни входни данни
+        Console.WriteLine("Wrong input!");
+    }
+}
+
+
+//проверяваме дали годината е високосна
 bool leap=DateTime.IsLeapYear(year);
+
+//ако е високосна
 if (leap) Console.WriteLine($"The year {year} is leap!");
+
+//ако не е високосна
 else Console.WriteLine($"The year {year} is NOT leap!");
