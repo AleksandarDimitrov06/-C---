908ce336ec7ae31c4059b81e101d668401606ad6
Александър Димитров <alexander.v.dim@gmail.com>
2024-02-16 18:58:43 +0200

    Changes on code

--- Намиране на прости числа/Program.cs
+++ Намиране на прости числа/Program.cs
@@ -1,14 +1,55 @@
-﻿Console.WriteLine("Enter number:");
-int num=int.Parse(Console.ReadLine());
-if (num < 0)
+﻿//проверяч на входните данни
+bool Check_Input = false;
+
+//променлива за входните данни
+string input = null;
+
+//променлива за числото
+int num = 0;
+
+while (!Check_Input)
 {
-    Console.WriteLine("The number must be positive!");
-    Environment.Exit(0);
+    //подканваме потребителя да въведе число
+    Console.WriteLine("Enter number:");
+
+    //взимаме входните данни
+    input = Console.ReadLine();
+
+
+    //проверяваме дали входните данни са валидни
+    if(int.TryParse(input, out num) && int.Parse(input) >= 0)
+    {
+        //прекратяваме цикъла
+        Check_Input = true;
+    }
+    
+    //в противен случай показваме съобщение за невалидни данни
+    else
+    {
+        Console.WriteLine("The number must be positive!");
+        
+    }
+    
 }
-for(int i = 0; i <= num; i++)
+
+//на конзолата принтираме всички прости числа по-малки или равни да зададеното
+
+
+for (int i = 0; i <= num; i++)
 {
+    //ако числото е по-малко от две няма резултат
+    if(num<2)
+    {
+        Console.WriteLine("None");
+
+        break;
+    }
+    
     if (Check(i)) Console.WriteLine($"{i} is prime number!");
 }
+
+
+//метод за намиране на просто число
 bool Check(int n)
 {
     if (n < 2) return false;
