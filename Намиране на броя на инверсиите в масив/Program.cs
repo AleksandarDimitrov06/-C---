9981d718efa589bb6ab972a0a6e293fa4a1e01b2
Александър Димитров <alexander.v.dim@gmail.com>
2024-02-18 15:14:24 +0200

    Changes on code

--- Намиране на броя на инверсиите в масив/Program.cs
+++ Намиране на броя на инверсиите в масив/Program.cs
@@ -1,12 +1,37 @@
-﻿Console.WriteLine("Enter the elements of the array with spaces between them");
-string input=Console.ReadLine();
-int[] array = input.Split(' ').Select(int.Parse).ToArray();
-int inversions = 0;
-for (int i = 0; i < array.Length-1; i++)
+﻿try
 {
-    for(int j=1; j < array.Length-1; j++)
+    //въвеждаме елементите на масива
+    Console.WriteLine("Enter the elements of the array with spaces between them");
+    string input = Console.ReadLine();
+    int[] array = input.Split(' ').Select(int.Parse).ToArray();
+
+    //променлива за инверсиите
+    int inversions = 0;
+
+    //обхождаме масива за всеки елемент
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        if (i < j && array[i] > array[j])inversions++;
+
+        //отново обхождаме масива
+        //това обхождане отговаря за намирането на инверсиите
+        for (int j = 1; j < array.Length - 1; j++)
+        {
+
+            //проверяваме за инверсия
+            if (i < j && array[i] > array[j])
+                //броя на инвверсиите се увеличава
+                inversions++;
+        }
     }
+
+    //показваме резултата
+    Console.WriteLine("Inversions: " + inversions);
 }
-Console.WriteLine("Inversions: "+inversions);
+
+catch (Exception)
+{
+
+    //ако прихванем грешка излизаме от програмтата
+    Console.WriteLine("Wrong input!");
+    Environment.Exit(1);
+}
