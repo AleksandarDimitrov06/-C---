c210f1aeb7e312e740a523e43bfd6a698df76878
Александър Димитров <alexander.v.dim@gmail.com>
2024-02-18 15:03:57 +0200

    Changes on code

--- Сортиране на масив по метода на мехурчето/Program.cs
+++ Сортиране на масив по метода на мехурчето/Program.cs
@@ -1,33 +1,67 @@
-﻿Console.WriteLine("Enter the length of the array");
-int length=int.Parse(Console.ReadLine());
-double[]array = new double[length];
-for(int i=0; i<length; i++)
+﻿try
 {
-    Console.WriteLine("Enter element number "+(i+1));
-    array[i]=double.Parse(Console.ReadLine());
+
+    //въвеждаме дължината на масива
+    Console.WriteLine("Enter the length of the array");
+    int length = int.Parse(Console.ReadLine());
+
+    //правим масива
+    double[] array = new double[length];
+
+    //за всеки елемент задаваме стойност от потребителя
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Enter element number " + (i + 1));
+        array[i] = double.Parse(Console.ReadLine());
+    }
+
+    //въвеждаме как иска потребителя да подреди масивва
+    Console.WriteLine("Enter how you want to sort your array 'asc'/'desc'");
+    string sort = Console.ReadLine();
+
+    //според това какво е въвел сортираме числата и ги показваме на конзолата
+    Console.WriteLine("Sorted array");
+    switch (sort)
+    {
+        //възходящо
+        case "asc":
+            Console.WriteLine(String.Join(", ", SortAsc(array)));
+            break;
+
+        //низходящо
+        case "desc":
+            Console.WriteLine(String.Join(", ", SortDesc(array)));
+            break;
+
+        //ако е въвел грешни данни излизаме от програмта
+        default:
+            Console.WriteLine("Incorrect input!");
+            Environment.Exit(0);
+            break;
+    }
+
 }
-Console.WriteLine("Enter how you want to sort your array 'asc'/'desc'");
-string sort=Console.ReadLine();
-Console.WriteLine("Sorted array");
-switch (sort)
+catch (Exception)
 {
-    case "asc":       
-        Console.WriteLine(String.Join(", ",SortAsc(array)));
-        break;
-    case "desc":
-        Console.WriteLine(String.Join(", ",SortDesc(array)));
-        break;
-    default:
-        Console.WriteLine("Incorrect input!");
-        Environment.Exit(0);
-        break;
+    //прихващаме грешка
+    Console.WriteLine("Error!");
+    Environment.Exit(0);
 }
+
+
+//метод за сортиране възходящо
 double[]SortAsc(double[] array)
 {
+
+    //обхождаме масива толко пъти, колкото елемента има 
     for (int i = 0; i < array.Length - 1; i++)
     {
+
+        //повторно обхождане, което ще отговаря за размяната на елементите
         for (int j = 0; j < array.Length - 1; j++)
         {
+
+            //ако текущия елемент е по-голям от следващия ги разменяме
             if (array[j] > array[j + 1])
             {
                 double tmp = array[j];
@@ -36,14 +70,23 @@ double[]SortAsc(double[] array)
             }
         }
     }
+
+    //връщаме сортирания масив
     return array;
 }
+
+
+//метод за сортиране низходящо
 double[] SortDesc(double[] array)
 {
+
+    //отново правим два цикъла за обхождане
     for (int i = 0; i < array.Length - 1; i++)
     {
         for (int j = 0; j < array.Length - 1; j++)
         {
+
+            //ако текущия елемент е по-малък от следващия ги разменяме
             if (array[j] < array[j + 1])
             {
                 double tmp = array[j];
@@ -52,5 +95,7 @@ double[] SortDesc(double[] array)
             }
         }
     }
+
+    //връщаме масива
     return array;
 }
