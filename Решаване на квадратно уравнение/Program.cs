1a4f677abec91600679ced7fb6c73e579c6471e9
Александър Димитров <alexander.v.dim@gmail.com>
2024-02-18 14:10:27 +0200

    Changes on code

--- Решаване на квадратно уравнение/Program.cs
+++ Решаване на квадратно уравнение/Program.cs
@@ -1,36 +1,63 @@
-﻿Console.WriteLine("Enter a:");
-double a=double.Parse(Console.ReadLine());
-if (a <= 0)
+﻿try
 {
-    Console.WriteLine("The coefficient before a must be higher than 0!");
-    Environment.Exit(0);
-}
-Console.WriteLine("Enter b:");
-double b = double.Parse(Console.ReadLine());
-if (b <= 0)
-{
-    Console.WriteLine("The coefficient before b must be higher than 0!");
-    Environment.Exit(0);
-}
-Console.WriteLine("Enter c:");
-double c = double.Parse(Console.ReadLine());
-if (c <= 0)
-{
-    Console.WriteLine("The coefficient before c must be higher than 0!");
-    Environment.Exit(0);
-}
-double discriminant = b * b - 4 * a * c;
-switch (discriminant)
-{
-    case < 0: Console.WriteLine("Тhere are no roots of the equation");break;
-    case 0:
-        double result = -b / (2.0 * a);
-        Console.WriteLine("There is one root and it is "+result); 
-        break;
+    //подканваме потребителя да въведе коефициент за а
+    Console.WriteLine("Enter a:");
 
-    default:
-        double result1 = (-b + Math.Sqrt(discriminant)) / (2.0 * a);
-        double result2 = (-b - Math.Sqrt(discriminant)) / (2.0 * a);
-        Console.WriteLine($"The roots of the equation are {result1} and {result2}");
-        break;
+    //присвояваме стойността
+    double a = double.Parse(Console.ReadLine());
+
+    //това се повтаря за b
+    Console.WriteLine("Enter b:");
+    double b = double.Parse(Console.ReadLine());
+
+    //както и за c
+    Console.WriteLine("Enter c:");
+    double c = double.Parse(Console.ReadLine());
+
+
+
+    //изчисляваме дискриминантата
+    double discriminant = b * b - 4 * a * c;
+
+
+    //според дискриминантата определяме корените
+    switch (discriminant)
+    {
+        //ако е по-малка от 0 няма корени
+        case < 0: Console.WriteLine("Тhere are no roots of the equation"); break;
+
+        //ако е 0 има само един корен
+        case 0:
+
+            //намираме корена
+            double result = -b / (2.0 * a);
+
+            //показваме го на екрана
+            Console.WriteLine("There is one root and it is " + result);
+            break;
+        //по подразбиране дискриминантата е по-голяма от 0 и има два корена
+        default:
+
+            //намираме корените
+            double result1 = (-b + Math.Sqrt(discriminant)) / (2.0 * a);
+            double result2 = (-b - Math.Sqrt(discriminant)) / (2.0 * a);
+
+            //показваме ги на конзолата
+            Console.WriteLine($"The roots of the equation are {result1} and {result2}");
+            break;
+    }
 }
+catch (Exception)
+{
+    //ако потребителят е въвел невалидни данни излизаме от програмата
+    Console.WriteLine("Wroong input");
+    Environment.Exit(1);
+}
+
+
+
+
+
+
+
+
